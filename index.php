bc7d6153f82089e4911e8b657d4c31d0578de1ab
Maria13579 <patymsr3@gmail.com>
2021-05-27 07:52:39 -0600

    b

---
+++ index.php
@@ -0,0 +1,15 @@
+
+   <?php  include('Materialize/css.php'); ?>
+
+ <div class="card-panel teal lighten-2 "><h1 class="center-align">Por favor Inicie Sesion</h1> </div>
+
+ <div class = "center">
+      <a  class ="center-classsffect waves-light btn " href= "inicio.php">Inicie Sesion</a> 
+      </div>
+     <br/>
+     <div class = "center">
+     <img class="responsive-img" src="Recursos/f1.jpg">
+     </div>
+    <script type="text/javascript" src ="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    <?php  include('Materialize/java.php'); ?>
---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+
+    $user=[
+            ['user'=>'maria','pass'=>'123'],
+            ['user'=>'administrador','pass'=>'456'],
+            ['user'=>'natalia','pass'=>'789']
+        ];
+    $usuario=$_POST['user'];
+    $password=$_POST['pass'];
+    $estado=false;
+    $cont=0;
+        
+    foreach($user as $u)
+    {
+        if($u['user']==$usuario && $u['pass']==$password)   
+        {
+            $estado=true;
+            $cont++;    
+        } 
+    }
+
+    
+    if($estado)
+    {
+        header('location:area.php'); 
+    }
+    else
+    {
+        header('location:index.php');   
+    }
+?>
